0d5626edfb3c5834baf0d17f6e77d756c2e22df3
Thomas Cormican <thomascormican123@gmail.com>
2025-06-10 23:25:23 +0100

    make navbar responsive

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { CiMenuBurger } from "react-icons/ci";
+import { IoIosClose } from "react-icons/io";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="flex justify-between items-center mt-4 mb-10 p-4 max-w-[1100px] w-full">
+    <nav className="flex justify-between z-10 items-center mt-4 mb-10 p-4 max-w-[1100px] w-full">
       <div className="flex items-center w-[200px] gap-2 hover:cursor-pointer text-3xl font-bold">
         <Image src="/download.png" alt="csprime logo" width={50} height={50} />
         CS Prime
@@ -28,15 +33,42 @@ function Navbar() {
       </div>
       <div>
         <ul className="flex justify-around hidden md:flex gap-4 w-[200px]">
-          <li className="border-gray-400 border-1 py-2 px-4 rounded-lg">
+          <li className="border-gray-400 border-1 py-2 px-4 text-center rounded-lg">
             Login
           </li>
           <li className="bg-black text-white py-2 px-4 rounded-lg">Sign Up</li>
         </ul>
       </div>
       <div className="text-3xl flex md:hidden">
-        <span>
+        <span onClick={() => setIsOpen((prev) => !prev)}>
           <CiMenuBurger />
+        </span>
+      </div>
+      <div
+        className={`flex md:hidden p-8 ${
+          !isOpen && "hidden"
+        } fixed justify-between top-0 left-0 w-full bg-white`}
+      >
+        <ul className="flex flex-col gap-4">
+          <Link href="/" onClick={() => setIsOpen(false)}>
+            <li className="hover:cursor-pointer">Home</li>
+          </Link>
+          <Link href="/modules" onClick={() => setIsOpen(false)}>
+            <li className="hover:cursor-pointer">Modules</li>
+          </Link>
+          <Link href="/topics" onClick={() => setIsOpen(false)}>
+            <li className="hover:cursor-pointer">Topics</li>
+          </Link>
+          <Link href="/analytics" onClick={() => setIsOpen(false)}>
+            <li className="hover:cursor-pointer">Analytics</li>
+          </Link>
+          <li className="border-gray-400 border-1 py-2 px-4 rounded-lg">
+            Login
+          </li>
+          <li className="bg-black text-white py-2 px-4 rounded-lg">Sign Up</li>
+        </ul>
+        <span className="text-4xl" onClick={() => setIsOpen((prev) => !prev)}>
+          <IoIosClose />
         </span>
       </div>
     </nav>

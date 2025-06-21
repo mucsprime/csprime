ff12a1c78c8f996a71211f8c4f58e168ac689220
Thomas Cormican <thomascormican123@gmail.com>
2025-06-21 23:14:14 +0100

    update chatbot and added in memory chat storage

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Page() {
-  return <div>about</div>;
+  return <div>About</div>;
 }
 
 export default Page;
--- frontend/app/modules/[slug]/page.tsx
+++ frontend/app/modules/[slug]/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import Link from "next/link";
 import module_info from "@/public/module_info.json";
+import module_topic_relations from "@/public/module_topic_relations.json";
+
+type ModuleTopicKey = keyof typeof module_topic_relations;
 
 export default async function Page({
   params,
@@ -11,6 +15,8 @@ export default async function Page({
 
   if (Object.keys(module_info).includes(slug.toUpperCase())) {
     const currentModule = module_info[moduleKey];
+    const moduleRelations =
+      module_topic_relations[moduleKey as ModuleTopicKey] || null;
 
     return (
       <div className="flex flex-col items-center">
@@ -45,6 +51,38 @@ export default async function Page({
             )}
           </ul>
         </div>
+        {moduleRelations && (
+          <div className="p-8 mt-8 max-w-[1000px] w-full rounded-lg border-1 border-gray-200">
+            <table>
+              <thead>
+                <tr>
+                  <th className="text-xl font-medium mb-2">Topic</th>
+                  <th className="text-xl font-medium mb-2 text-center">
+                    Related Modules
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {Object.entries(moduleRelations).map((entry) => (
+                  <tr key={entry[0]}>
+                    <td>{entry[0]}</td>
+                    <td className="flex flex-wrap">
+                      {entry[1].map((relatedModule) => (
+                        <Link
+                          href={`/modules/${relatedModule}`}
+                          key={entry[0] + relatedModule}
+                          className="p-2 m-2 bg-gray-100 rounded-lg"
+                        >
+                          <span>{relatedModule}</span>
+                        </Link>
+                      ))}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
       </div>
     );
   }
